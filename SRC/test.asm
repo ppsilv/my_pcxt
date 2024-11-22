f19417941888dd9d2f68dc102507e15a66b7b246
pdsilva <ppsilv@gmail.com>
2024-11-22 14:00:34 -0300

    Teste de existenciad e memoria

--- SRC/test.asm
+++ SRC/test.asm
@@ -13,26 +13,78 @@ reset:
             
     		cli     ; Clear Interrupts until vectors defined
             cld
+
+        mov ax, 0x0000
+        mov es, ax
+        mov ss, ax
+        mov ax, 0xF000
+        mov ds, ax
+		mov cs, ax
+        ;Put 0x0000 in stack pointer
+        xor sp, sp
+
+        mov ax, 0xAA55
+        mov word es:[0x0], ax
+
+        xor ax,ax
+
+        mov ax,word es:[0x0] 
+        cmp ax, 0xAA55
+        jnz  .loop1   
+
 .loop:            
             mov     al, 0x00
             out     0x80, al
             out     0x80, al
 
-            mov     cx, 0x7fff
+            mov     cx, 0xffff
 .label01:
             dec     cx
             jnz     .label01
+            mov     cx, 0xffff
+.label012:
+            dec     cx
+            jnz     .label012
 
             mov     al, 0x01
             out     0x80, al
             out     0x80, al
 
-            mov     cx, 0x7fff
+            mov     cx, 0xffff
 .label02:
             dec     cx
             jnz     .label02
+            mov     cx, 0xffff
+.label022:
+            dec     cx
+            jnz     .label022
 
             jmp     .loop    
+
+
+.loop1:            
+            mov     al, 0x00
+            out     0x80, al
+            out     0x80, al
+
+            mov     cx, 0x3fff
+.label011:
+            dec     cx
+            jnz     .label011
+
+            mov     al, 0x01
+            out     0x80, al
+            out     0x80, al
+
+            mov     cx, 0x3fff
+.label021:
+            dec     cx
+            jnz     .label021
+
+            jmp     .loop1    
+
+
+
 
 
             hlt     ;stops cpu    
